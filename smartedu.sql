191af826ad6abb822d09a5c1c4cb65bd240ee3a5
farhanizz21 <43459600+farhanizz21@users.noreply.github.com>
2025-03-11 13:22:08 +0700

    add db via upload

---
+++ smartedu.sql
@@ -0,0 +1,450 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 11, 2025 at 07:21 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smartedu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_by` varchar(100) NOT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guru`
+--
+
+CREATE TABLE `guru` (
+  `id` int(100) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `mapel_uuid` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `jenis_kelamin` int(5) NOT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelompok`
+--
+
+CREATE TABLE `kelompok` (
+  `id` int(50) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `proyek_uuid` varchar(100) NOT NULL,
+  `kelompok` varchar(100) NOT NULL,
+  `created_by` varchar(100) NOT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelompok_siswa`
+--
+
+CREATE TABLE `kelompok_siswa` (
+  `id` int(50) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `kelompok_uuid` varchar(100) NOT NULL,
+  `siswa_uuid` varchar(100) NOT NULL,
+  `created_by` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mapel`
+--
+
+CREATE TABLE `mapel` (
+  `id` int(50) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `materi`
+--
+
+CREATE TABLE `materi` (
+  `id` int(50) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `mapel_uuid` varchar(100) DEFAULT NULL,
+  `judul` varchar(100) NOT NULL,
+  `thumbnail` varchar(100) DEFAULT NULL,
+  `berkas` varchar(100) NOT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `panduan`
+--
+
+CREATE TABLE `panduan` (
+  `id` int(50) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `berkas` varchar(100) NOT NULL,
+  `tujuan` varchar(100) NOT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proyek`
+--
+
+CREATE TABLE `proyek` (
+  `id` int(11) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `mapel_uuid` varchar(100) DEFAULT NULL,
+  `file` varchar(100) NOT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `tgl_mulai` datetime NOT NULL,
+  `tgl_selesai` datetime NOT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proyek_jawaban`
+--
+
+CREATE TABLE `proyek_jawaban` (
+  `id` int(11) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `proyek_uuid` varchar(100) NOT NULL,
+  `kelompok_uuid` varchar(100) NOT NULL,
+  `kelompok_nama` varchar(100) DEFAULT NULL,
+  `jawaban_text` text DEFAULT NULL,
+  `jawaban_file` varchar(100) DEFAULT NULL,
+  `nilai` float DEFAULT NULL,
+  `created_by` varchar(100) NOT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id` int(50) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `nis` int(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `tgl_lahir` datetime NOT NULL,
+  `jenis_kelamin` int(5) NOT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ujian`
+--
+
+CREATE TABLE `ujian` (
+  `id` int(50) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `mapel_uuid` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `tgl_mulai` datetime NOT NULL,
+  `tgl_selesai` datetime NOT NULL,
+  `created_by` varchar(100) NOT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ujian_jawaban`
+--
+
+CREATE TABLE `ujian_jawaban` (
+  `id` int(50) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `ujian_uuid` varchar(100) NOT NULL,
+  `soal_uuid` varchar(100) NOT NULL,
+  `jawaban_siswa` varchar(100) NOT NULL,
+  `nilai` int(100) DEFAULT NULL,
+  `created_by` varchar(100) NOT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ujian_siswa`
+--
+
+CREATE TABLE `ujian_siswa` (
+  `id` int(11) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `ujian_uuid` varchar(100) NOT NULL,
+  `siswa_uuid` varchar(100) NOT NULL,
+  `ujian_nilai` double DEFAULT NULL,
+  `created_by` varchar(100) NOT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ujian_soal`
+--
+
+CREATE TABLE `ujian_soal` (
+  `id` int(50) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
+  `ujian_uuid` varchar(100) NOT NULL,
+  `soal` text NOT NULL,
+  `created_by` varchar(100) NOT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `guru`
+--
+ALTER TABLE `guru`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kelompok`
+--
+ALTER TABLE `kelompok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kelompok_siswa`
+--
+ALTER TABLE `kelompok_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mapel`
+--
+ALTER TABLE `mapel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `materi`
+--
+ALTER TABLE `materi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `panduan`
+--
+ALTER TABLE `panduan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `proyek`
+--
+ALTER TABLE `proyek`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `proyek_jawaban`
+--
+ALTER TABLE `proyek_jawaban`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ujian`
+--
+ALTER TABLE `ujian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ujian_jawaban`
+--
+ALTER TABLE `ujian_jawaban`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ujian_siswa`
+--
+ALTER TABLE `ujian_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ujian_soal`
+--
+ALTER TABLE `ujian_soal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `guru`
+--
+ALTER TABLE `guru`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kelompok`
+--
+ALTER TABLE `kelompok`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kelompok_siswa`
+--
+ALTER TABLE `kelompok_siswa`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mapel`
+--
+ALTER TABLE `mapel`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `materi`
+--
+ALTER TABLE `materi`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `panduan`
+--
+ALTER TABLE `panduan`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `proyek`
+--
+ALTER TABLE `proyek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `proyek_jawaban`
+--
+ALTER TABLE `proyek_jawaban`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ujian`
+--
+ALTER TABLE `ujian`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ujian_jawaban`
+--
+ALTER TABLE `ujian_jawaban`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ujian_siswa`
+--
+ALTER TABLE `ujian_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ujian_soal`
+--
+ALTER TABLE `ujian_soal`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
