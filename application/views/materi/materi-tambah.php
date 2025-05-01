72b2257ed548e0135a357468613fa7a243e74e90
rizalabulfata <rizalabulfata@gmail.com>
2025-05-02 04:28:30 +0700

    (fix) get uuid by url path handle guru return multiple uuid

--- application/views/materi/materi-tambah.php
+++ application/views/materi/materi-tambah.php
@@ -1,8 +1,12 @@
+<?php
+$uuid = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+?>
 <div class="container-fluid">
     <?php if ($this->session->flashdata('error_msg')) : ?>
-    <div class="alert alert-danger">
-        <?= $this->session->flashdata('error_msg'); ?>
-    </div>
+        <div class="alert alert-danger">
+            <?= $this->session->flashdata('error_msg'); ?>
+        </div>
     <?php endif; ?>
 
 
@@ -12,7 +16,7 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item">
-                <a href="<?= base_url('materi')?>">
+                <a href="<?= base_url('materi') ?>">
                     <i class="fas fa-arrow-left">
                     </i> Daftar Materi</a>
             </li>
@@ -23,15 +27,15 @@
     <div class="card shadow mb-4">
         <div class="card-body">
             <form class="user" method="post" enctype="multipart/form-data"
-                action="<?= base_url('materi/tambah/'.$guru->mapel_uuid);?>">
-                <input type="hidden" name="namaMapel" value="<?= $guru->mapel_uuid; ?>">
+                action="<?= base_url('materi/tambah/' . $uuid); ?>">
+                <input type="hidden" name="namaMapel" value="<?= $uuid; ?>">
                 <div class="form-group row">
                     <div class="col-sm-6">
                         <label class="form-label font-weight-bold">Thumbnail</label>
                         <input type="file" name="thumbnail" id="thumbnail" class="form-control"
                             placeholder="Masukkan Thumbnail" value="<?= set_value('thumbnail'); ?>">
                         <small>format file : JPG, JPEG, PNG. Maximal 2Mb</small>
-                        <div class="invalid-feedback <?= !empty(form_error('thumbnail')) ? 'd-block' : '' ; ?> ">
+                        <div class="invalid-feedback <?= !empty(form_error('thumbnail')) ? 'd-block' : ''; ?> ">
                             <?= form_error('thumbnail') ?>
                         </div>
                     </div>
@@ -41,7 +45,7 @@
                         <label class="form-label font-weight-bold">Judul <span class="text-danger">*</span></label>
                         <input type="text" name="judul" id="judul" class="form-control"
                             placeholder="Masukkan Judul Materi" value="<?= set_value('judul'); ?>">
-                        <div class="invalid-feedback <?= !empty(form_error('judul')) ? 'd-block' : '' ; ?> ">
+                        <div class="invalid-feedback <?= !empty(form_error('judul')) ? 'd-block' : ''; ?> ">
                             <?= form_error('judul') ?>
                         </div>
                     </div>
@@ -53,7 +57,7 @@
                         <input type="file" name="berkas" id="berkas" class="form-control"
                             placeholder="Masukkan materi Materi" value="<?= set_value('berkas'); ?>">
                         <small>format file : PDF, DOCX, PPTX, Video. Maximal 50Mb</small>
-                        <div class="invalid-feedback <?= !empty(form_error('berkas')) ? 'd-block' : '' ; ?> ">
+                        <div class="invalid-feedback <?= !empty(form_error('berkas')) ? 'd-block' : ''; ?> ">
                             <?= form_error('berkas') ?>
                         </div>
                     </div>
@@ -63,7 +67,7 @@
                         <button type="submit" class="btn btn-md btn-success mr-2">
                             <i class="fa fa-save"></i> Simpan
                         </button>
-                        <a href="<?= base_url('Materi')?>" class="btn btn-md btn-danger">
+                        <a href="<?= base_url('Materi') ?>" class="btn btn-md btn-danger">
                             <i class="fa fa-times"></i> Batal
                         </a>
                     </div>
